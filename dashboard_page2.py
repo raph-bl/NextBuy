e9be2b0fa7394c9817c3d4a116174e9ab03ebe62
tara-nour <tara-nour.monteiro@epitech.eu>
2026-03-11 10:10:40 +0100

    feat(dashboard): initiliazed dashboard and added one graph

---
+++ dashboard_page2.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd 
+# dataframes 
+aisles         = pd.read_csv('./datasets/aisles.csv')
+departments    = pd.read_csv('./datasets/departments.csv')
+products       = pd.read_csv('./datasets/products.csv')
+orders         = pd.read_csv('./datasets/orders.csv')
+order_products = pd.read_csv('./datasets/order_products.csv')
+
+st.markdown("# page 2")
+st.sidebar.markdown("# Page 2")
---
+++ nextbuy_dashboard.py
@@ -0,0 +1,29 @@
+import streamlit as st 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+# dataframes 
+aisles         = pd.read_csv('./datasets/aisles.csv')
+departments    = pd.read_csv('./datasets/departments.csv')
+products       = pd.read_csv('./datasets/products.csv')
+orders         = pd.read_csv('./datasets/orders.csv')
+order_products = pd.read_csv('./datasets/order_products.csv')
+# dashboard base
+main_page = st.Page("nextbuy_dashboard.py", title="main page")
+page_2    = st.Page("dashboard_page2.py", title="product reorder prediction")
+pg        = st.navigation([main_page, page_2])
+
+# code 
+st.title("Welcome to NextBuy!")
+st.subheader('Most popular product')
+
+cart_first=order_products[order_products['add_to_cart_order']==1]
+count_first_product=cart_first['product_id'].value_counts().reset_index()
+count_first_product.columns=['product_id', 'count']
+top_merged=pd.merge(count_first_product, products[['product_id', 'product_name']], on='product_id', how='left')
+top = top_merged.sort_values('count', ascending=True).tail(10)
+st.bar_chart(
+    data=top.set_index('product_name')['count'],
+    horizontal=True,
+    color="#6a0dad"
+)
