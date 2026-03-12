e44c6dcf6a45cc75bbf6a90dec8d2983ff185e56
tara-nour <tara-nour.monteiro@epitech.eu>
2026-03-12 11:24:38 +0100

    feat(select box for product reorder prediction): added selection of product and user id

--- dashboard_page2.py
+++ dashboard_page2.py
@@ -1,11 +1,28 @@
 import streamlit as st
 import pandas as pd 
+import joblib
 # dataframes 
-aisles         = pd.read_csv('./datasets/aisles.csv')
-departments    = pd.read_csv('./datasets/departments.csv')
-products       = pd.read_csv('./datasets/products.csv')
-orders         = pd.read_csv('./datasets/orders.csv')
-order_products = pd.read_csv('./datasets/order_products.csv')
 
-st.markdown("# page 2")
+@st.cache_data
+def load_data():
+    aisles         = pd.read_csv('./datasets/aisles.csv')
+    departments    = pd.read_csv('./datasets/departments.csv')
+    products       = pd.read_csv('./datasets/products.csv')
+    orders         = pd.read_csv('./datasets/orders.csv')
+    order_products = pd.read_csv('./datasets/order_products.csv')
+    return aisles, departments, products, orders, order_products
+aisles, departments, products, orders, order_products = load_data()
+# model
+model   = joblib.load('./models/random_forest.pkl')
+model_2 = joblib.load('./models/logistic_regression.pkl')
 
+st.markdown("# Product reorder prediction")
+prod_options = products['product_name'].tolist()
+prod_user    = orders['user_id'].unique().tolist()
+with st.form("prediction_name"):
+    selected_prod = st.selectbox("product", prod_options)
+    selected_user = st.selectbox("user", prod_user)
+    submit        = st.form_submit_button("prédire les chances de rachat")
+
+if submit:
+    prod_id = products[products['product_name'] == selected_prod]['product_id'].values[0]
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 # def des pages
-main_page = st.Page("nextbuy_dashboard.py", title="Main Page", icon="🏠")
-page_2    = st.Page("dashboard_page2.py", title="Product Reorder Prediction", icon="📈")
+main_page = st.Page("nextbuy_dashboard.py", title="Main Page")
+page_2    = st.Page("dashboard_page2.py", title="Product Reorder Prediction")
 
 pg = st.navigation([main_page, page_2])
 pg.run()
